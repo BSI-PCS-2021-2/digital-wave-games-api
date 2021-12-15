72169b0608d1b470beb175b2f7942f23348c85ec
pedro clain <pedroclain@edu.unirio.br>
2021-12-15 17:05:25 -0300

    General refactoring caart endpoints

--- src/routes/store/cart.routes.ts
+++ src/routes/store/cart.routes.ts
@@ -5,22 +5,19 @@ import { cartsController } from '../../controllers';
 const router = Router();
 
 router.get('/cart/:cart_id/items', (request, response) => {
-    console.log("On GET endpoint")
     return cartsController.getCartItems(request, response);
 });
 
+
 router.post('/cart/items', (request, response) => {
-    console.log("On POST endpoint")
     return cartsController.postCartItem(request, response);
 });
 
 router.put('/cart/items/:item_id', (request, response) => {
-    console.log("On PUT endpoint")
     return cartsController.putCartItem(request, response);
 });
 
 router.delete('/cart/items/:item_id', (request, response) => {
-    console.log("On DELETE endpoint")
     return cartsController.deleteCartItem(request, response);
 });
 
--- src/services/authentication.service.ts
+++ src/services/authentication.service.ts
@@ -24,16 +24,15 @@ export class AuthenticationService {
             if (dbPassword === sha256(password + ENCRYPTION_SECRET).toString()) {
                 const jwtBearerToken = jsonwebtoken.sign({}, RSA_PRIVATE_KEY, {
                     algorithm: 'RS256',
-                    expiresIn: 3600,
-                    subject: username,
-                    
+                    expiresIn: 86400, // um dia
+                    subject: username
                 });
                 
                 
 
                 jwt = {
                     idToken: jwtBearerToken, 
-                    expiresIn: 60,
+                    expiresIn: 86400,
                     username: username,
                     userId: user?.id
                 };

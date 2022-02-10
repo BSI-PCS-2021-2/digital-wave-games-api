2833cb72b9427bec90ede9c9fbbb71ce32f10b23
Breno Sousa <breno.sousa@uniriotec.br>
2022-02-10 19:52:40 -0300

    Add order e-mail notification

--- src/interfaces/productsRepository.interface.ts
+++ src/interfaces/productsRepository.interface.ts
@@ -1,16 +1,21 @@
-import { Product, PostProductDTO, PutProductDTO, Gender, Platform, Publisher, RatingSystem } from '../models';
+import {
+  Product,
+  PostProductDTO,
+  PutProductDTO,
+  Gender,
+  Platform,
+  Publisher,
+  RatingSystem,
+} from "../models";
 
 export interface IProductsRepository {
-
-    getProducts(): Promise<Product[]>;
-    getById(id: number): Promise<Product | null>;
-    postProduct(dto: PostProductDTO): Promise<number[] | null>;
-    putProduct(dto: PutProductDTO): Promise<void>;
-    deleteProduct(id: number): Promise<void>;
-    getGender(id: number): Promise<Gender | undefined>;
-    getRatingSystem(id: number): Promise<RatingSystem | undefined>;
-    getPublisher(id: number): Promise<Publisher | undefined>;
-    getPlatform(id: number): Promise<Platform | undefined>;
-
-
+  getProducts(): Promise<Product[]>;
+  getById(id: number | undefined): Promise<Product | null>;
+  postProduct(dto: PostProductDTO): Promise<number[] | null>;
+  putProduct(dto: PutProductDTO): Promise<void>;
+  deleteProduct(id: number): Promise<void>;
+  getGender(id: number): Promise<Gender | undefined>;
+  getRatingSystem(id: number): Promise<RatingSystem | undefined>;
+  getPublisher(id: number): Promise<Publisher | undefined>;
+  getPlatform(id: number): Promise<Platform | undefined>;
 }
